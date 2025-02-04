3b0a3ccd4f19e91d13fadcb23c5dfa97fff170c5
Ground <mattguarneros@gmail.com>
2025-02-04 08:20:02 -0800

    didn't Fondue it this time lol

---
+++ Create_Token_files/util.js
@@ -0,0 +1,45 @@
+var $_GET = {};
+
+/* Gets all GET variables encoded into the url */
+function get_vars() {
+
+    if(document.location.toString().indexOf('?') !== -1) {
+        var query = document.location
+        .toString()
+        // get the query string
+        .replace(/^.*?\?/, '')
+        // and remove any existing hash string (thanks, @vrijdenker)
+        .replace(/#.*$/, '')
+        .split('&');
+
+        for(var i=0, l=query.length; i<l; i++) {
+            var aux = decodeURIComponent(query[i]).split('=');
+            $_GET[aux[0]] = aux[1];
+        }
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+get_vars();
